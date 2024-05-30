35ba6faaa38c15ea436c9077935dba91c9e72207
MagicSakuraD <ckgoforit@outlook.com>
2024-05-30 16:50:09 +0800

    chore: Add UploadModal component and update npm dependencies

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,9 +1,21 @@
 "use client";
 import { TbPlaylist } from "react-icons/tb";
 import { AiOutlinePlus } from "react-icons/ai";
+import { useUser } from "@/hooks/useUser";
+import useAuthModal from "@/hooks/useAuthModal";
+import useUploadModal from "@/hooks/useUploadModal";
 
 const Library = () => {
-  const onClick = () => {};
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+  const onClick = () => {
+    if (!user) {
+      authModal.onOpen();
+    }
+    //TODO: check for subscription
+    return uploadModal.onOpen();
+  };
   return (
     <div className="flex flex-col">
       <div
