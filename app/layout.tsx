687a107a1f407a9b5c58cff2b9ec600e4f32fc60
MagicSakuraD <ckgoforit@outlook.com>
2024-05-30 09:49:28 +0800

    chore: Update npm dependencies for Next.js and React

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ModeToggle";
 import SupabaseProvider from "@/providers/SupabaseProvider";
+import { User } from "lucide-react";
+import UserProvider from "@/providers/UserProvider";
 const font = Figtree({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -28,7 +30,9 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <SupabaseProvider>
-            <Sidebar>{children}</Sidebar>
+            <UserProvider>
+              <Sidebar>{children}</Sidebar>
+            </UserProvider>
           </SupabaseProvider>
         </ThemeProvider>
       </body>
