e8812b9787fbd3e8cb9d5511934058c97b6ddd96
MagicSakuraD <ckgoforit@outlook.com>
2024-06-07 16:45:25 +0800

    chore: Update Image component to use fill prop in Liked page and MediaItem component

--- app/(site)/liked/page.tsx
+++ app/(site)/liked/page.tsx
@@ -15,10 +15,11 @@ const Liked = async () => {
           <div className="flex flex-col md:flex-row items-center gap-x-5">
             <div className="relative h-32 w-32 lg:h-44 lg:w-44">
               <Image
-                fill
+                fill={true}
                 alt="Playlist"
                 src="/images/liked.png"
                 className="object-cover"
+                sizes="100%"
               />
             </div>
             <div className="flex flex-col gap-y-2 mt-4 md:mt-0">
--- app/account/components/AccountContent.tsx
+++ app/account/components/AccountContent.tsx
@@ -1,7 +1,54 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+// import { supabase } from "@/utils/supabase/client";
+
+// import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@supabase/supabase-js";
+
 import React from "react";
 
-const AccountContent = () => {
-  return <div>AccountContent</div>;
+// async function getData() {
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
+//   const supabase = createClient(
+//     process.env.NEXT_PUBLIC_SUPABASE_URL!,
+//     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+//   );
+
+//   const {
+//     data: { user },
+//     error,
+//   } = await supabase.auth.getUser();
+
+//   console.log(user);
+
+//   if (error) {
+//     // This will activate the closest `error.js` Error Boundary
+//     console.log(error);
+//     throw new Error("Failed to fetch data");
+//   }
+
+//   return user;
+// }
+
+const AccountContent = async () => {
+  // const user = await getData();
+  return (
+    <Card className="container mx-auto mt-6">
+      <CardHeader>
+        <CardTitle className="">Account Information</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>Comming soon!</p>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default AccountContent;
